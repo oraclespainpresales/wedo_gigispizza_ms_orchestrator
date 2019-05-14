20307ba125f5971b9060b2f0019cfc8a146572f8
fharris <fernando.harris@gmail.com>
2019-05-14 16:22:00 +0200

    corrected async orchestration after fn call.

--- index.js
+++ index.js
@@ -50,59 +50,63 @@ app.post('/createOrder', async (req, res) => {
 
     let paymentMethod = req.body.payment.paymentMethod;
     let totalPayed = req.body.payment.totalPayed;
-    
+
     var fnInvokeEndpoint = "https://kfd4yc7wzsq.us-phoenix-1.functions.oci.oraclecloud.com/20181201/functions/ocid1.fnfunc.oc1.phx.aaaaaaaaabbnp3n4nvk4hxmldnxhkj2ptt62hhucrsqocaryfu6lut5ytyma/actions/invoke";
     var context = yaml.load('fn-node-invokebyendpoint/config.yaml') // load OCI context values
     var keyPath = context.privateKeyPath
-      if (keyPath.indexOf('~/') === 0) {
+    if (keyPath.indexOf('~/') === 0) {
         keyPath = keyPath.replace('~', os.homedir())
-      }
-      
-      // read the private key
-      fs.readFile(keyPath, 'ascii', (err, data) => {
+    }
+
+    // read the private key
+    fs.readFile(keyPath, 'ascii', (err, data) => {
         if (err) {
-          console.error("Can't read keyfile: " + keyPath)
-          process.exit(-1)
+            console.error("Can't read keyfile: " + keyPath)
+            process.exit(-1)
         }
         context.privateKey = data
-      
-        if (paymentMethod == "AMEX"){
-                functions.invokeFunction(context, fnInvokeEndpoint, totalPayed, function (response) {
-            console.log("functionResponse :" + response)
-            // Change the valueof payment.totalPayed
-             payment.totalPayed = response;
-             console.log("Total to pay after discount applied :" +  payment.totalPayed + "$" );           
-            })  
-        }else  console.log("Not eligible to discount");
-});
 
-    //TODO OtherDBs payload
-    //let customerAdress = req.body.customerAdress
-    //Do not forget to put the metadata on nodeJS
-    adapters.use(config.jsondb.insert, order).then((resJSONDB) => {
-        
-        
+        if (paymentMethod == "AMEX") {
+            functions.invokeFunction(context, fnInvokeEndpoint, totalPayed, function (response) {
+                console.log("functionResponse :" + response)
+                // Change the valueof payment.totalPayed
+                payment.totalPayed = response;
 
-        adapters.use(config.sqldb.insert, payment).then((resSQLDB) => {
+                //TODO OtherDBs payload
+                //let customerAdress = req.body.customerAdress
+                //Do not forget to put the metadata on nodeJS
+                adapters.use(config.jsondb.insert, order).then((resJSONDB) => {
 
-            //TO remove once you added the call to GraphDB
-            res.send({"resJSONDB": resJSONDB, "resSQLDB": resSQLDB});
-/*
-            adapters.use(config.graphdb.insert, payment).then((resGraphDB) => {
 
-                res.send({"resJSONDB": resJSONDB, "resSQLDB": resSQLDB, "resGraphDB" : resGraphDB});
-        
-            }).catch((err) => {
-                console.log("Error: " + err)
+                    console.log("Total to pay after discount applied (1***):" + payment.totalPayed + "$");
+                    adapters.use(config.sqldb.insert, payment).then((resSQLDB) => {
+
+                        //TO remove once you added the call to GraphDB
+                        res.send({ "resJSONDB": resJSONDB, "resSQLDB": resSQLDB });
+                        /*
+                                    adapters.use(config.graphdb.insert, payment).then((resGraphDB) => {
+                        
+                                        res.send({"resJSONDB": resJSONDB, "resSQLDB": resSQLDB, "resGraphDB" : resGraphDB});
+                                
+                                    }).catch((err) => {
+                                        console.log("Error: " + err)
+                                    })
+                        */
+                    }).catch((err) => {
+                        console.log("Error: " + err)
+                    })
+
+                }).catch((err) => {
+                    console.log("Error: " + err)
+                })
+
+                console.log("Total to pay after discount applied :" + payment.totalPayed + "$");
+                console.log("Total to pay after discount applied :" + payment.totalPayed + "$");
             })
-*/
-        }).catch((err) => {
-            console.log("Error: " + err)
-        })
+        } else console.log("Not eligible to discount");
+    });
 
-    }).catch((err) => {
-        console.log("Error: " + err)
-    })
+
 });
 //##############################  End - JSON DB #################################
 
